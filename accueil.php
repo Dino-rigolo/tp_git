e79e5bd42a14741a9b2a49c6575b17b24919a64a
Isaure Boiteau <isaure.boiteau@etu.unicaen.fr>
2025-02-12 18:22:53 +0100

    modif header

--- accueil.php
+++ accueil.php
@@ -3,31 +3,27 @@
 <head>
    <title>Page d'accueil</title>
    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+   <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
+   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </head>
 <body>
    <div class="container">
-         <!-- <div class="container">
-            <nav class="navbar navbar-dark bg-dark">
-               <a class="navbar-brand" href="accueil.php">Accueil</a>
-               <a class="navbar-brand" href="contact.php">Contact</a>
-            </nav>
-         </div> -->
-
          <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
             <a class="navbar-brand" href="accueil.php">Accueil</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                <span class="navbar-toggler-icon"></span>
             </button>
 
-               <div class="collapse navbar-collapse" id="navbarNav">
-                  <ul class="navbar-nav">
-                     <li class="nav-item active">
-                        <a class="nav-link" href="accueil.php">Accueil <span class="sr-only">(current)</span></a>
-                     </li>
-                     <li class="nav-item">
-                        <a class="nav-link" href="contact.php">Contact</a>
-                     </li>
-                  </ul>
+            <div class="collapse navbar-collapse" id="navbarNav">
+               <ul class="navbar-nav">
+                  <li class="nav-item active">
+                     <a class="nav-link" href="accueil.php">Accueil <span class="sr-only">(current)</span></a>
+                  </li>
+                  <li class="nav-item">
+                     <a class="nav-link" href="contact.php">Contact</a>
+                  </li>
+               </ul>
             </div>
          </nav>
       <h1>Accueil</h1>
