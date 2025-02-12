115a4ebeb7899183a8709ca8dbc1da0ab5c35fe2
Isaure Boiteau <isaure.boiteau@etu.unicaen.fr>
2025-02-12 18:20:06 +0100

    yo

--- accueil.php
+++ accueil.php
@@ -15,9 +15,9 @@
 
          <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
             <a class="navbar-brand" href="accueil.php">Accueil</a>
-               <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                  <span class="navbar-toggler-icon"></span>
-               </button>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+               <span class="navbar-toggler-icon"></span>
+            </button>
 
                <div class="collapse navbar-collapse" id="navbarNav">
                   <ul class="navbar-nav">
